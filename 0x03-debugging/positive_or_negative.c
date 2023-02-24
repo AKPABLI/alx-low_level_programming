71b2569e5675d52744563de3d817e4fe5aaa8386
vincent <vincent.akpabli@yahoo.com>
2023-02-24 06:13:57 -0800

    0-task

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -10,8 +10,8 @@
 {
 	if (i > 0)
 		printf("%d is positive\n", i);
-	else if (i == 0)
-		printf("%d is zero\n", i);
-	else
+	else if (i < 0)
 		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
 }
