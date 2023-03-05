7bd692542bddeeab25e5e073cd15d22ad66a8aec
vincent <vincent.akpabli@yahoo.com>
2023-03-05 15:18:25 -0800

    101-crackme_password

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,8 +2,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-
 char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -22,5 +20,5 @@ void print_diagsums(int *a, int size);
 
 void set_string(char **s, char *to);
 
-
+int _putchar(char c);
 #endif
